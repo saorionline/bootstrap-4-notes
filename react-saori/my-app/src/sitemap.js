9b2e8cbdf5c17c05a132ae52715258dd66e80af4
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2023-06-08 15:18:34 -0500

    version means sitemap refactoring

--- react-saori/my-app/src/sitemap.js
+++ react-saori/my-app/src/sitemap.js
@@ -1,53 +1,51 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 function Sitemap() {
     return (
-        /*
         <nav>
             <ul>
-                <li id="highInt">Tienda
-                    <ul class="inner-menu">
-                        <li id="me">Café Especial
-                                <ul class="inner-menu">
-                                    <li id="me">Blends</li>
-                                    <li id="me">Single Origin</li>
-                                    <li id="me">Cold Bred</li>
-                                    <li id="me">Kits</li>
-                                    <li id="me">Métodos</li>
-                                </ul>
-                        </li>
-                        <li>Merch</li>
-                    </ul>
-                </li>  
-            </ul>
-        </nav>   
-        Taken from https://codesandbox.io/p/github/saorionline/Coffee-Shop/BackGround */      
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/"> Café Especial</Link>
-                    <ul>
-                        <li>
-                            <Link to="/pages/blend">Blends</Link>
-                        </li>
-                        <li>
-                            <Link to="/pages/single">Single Origin</Link>
-                        </li>
-                        <li>
-                            <Link to="/pages/coldbred">Cold Bred</Link>
-                        </li>                        
-                        <li>
-                            <Link to="/pages/kit">Kits</Link>
-                        </li>
-                        <li>
-                            <Link to="/pages/barista">Barista</Link>
-                        </li>                                                
-                    </ul>
-                </li>
-            </ul>
-        </nav>   
+                {routes.map(route => (
+                    <li>
+                        
+                    <NavLink 
+                      style={({ isActive}) => ({
+                        color: isActive ? 'salmon' : 'pink',
+                        })}
+                        to={route.to}
+                        >
+                        {route.text}
+                        </NavLink>
+                    </li>
+                ))}  
+            </ul> 
+        </nav>          
     );
 }
+const routes = [];
+routes.push({
+    to: "/",
+    text: "Café Especial",
+});
+routes.push({
+    to: "/pages/blend",
+    text: "Blends",
+});
+routes.push({
+    to: "/pages/single",
+    text: "Single Origin",
+});
+routes.push({
+    to: "/pages/coldbred",
+    text: "Cold Bred",
+});
+routes.push({
+    to: "/pages/kit",
+    text: "Kits",
+});
+routes.push({
+    to: "/pages/barista",
+    text: "Barista",
+});
 
 export {Sitemap}
